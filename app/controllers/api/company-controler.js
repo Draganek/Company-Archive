041499f700a625907447aa473541dcdab7d0a4d1
Radosław Draganek <radek.draganek@gmail.com>
2025-03-12 23:58:39 +0100

    Api progress

--- app/controllers/api/company-controler.js
+++ app/controllers/api/company-controler.js
@@ -1,4 +1,5 @@
 const Company = require('../../db/models/company')
+const fs = require('fs')
 
 
 class CompanyController {
@@ -26,7 +27,7 @@ class CompanyController {
 
     async edit(req, res) {
         const { slug } = req.params;
-        const company = await Company.findOne({ slug })
+        const company = await Company.findOne({ slug });
         if (req.body.name) company.name = req.body.name;
         if (req.body.slug) company.slug = req.body.slug;
         if (req.body.employeesCount) company.employeesCount = req.body.employeesCount;
@@ -40,12 +41,26 @@ class CompanyController {
 
         try {
             await company.save();
-            res.status(201).json(company)
+            res.status(200).json(company)
         } catch (e) {
             res.status(422).json({ errors: e.errors });
         }
-
     }
+
+    async delete(req, res) {
+        const { slug } = req.params;
+        const company = await Company.findOne({ slug })
+        try {
+          if (company.image) {
+            fs.unlinkSync('public/uploads/' + company.image)
+          }
+          await Company.deleteOne({ slug });
+          res.status(204).send();
+        }
+        catch (e) {
+    
+        }
+      }
 
 }
 
--- app/routes/api.js
+++ app/routes/api.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = new express.Router();
 const CompanyController = require('../controllers/api/company-controler')
+const upload = require('../services/uploader')
 
 router.get('/companies', CompanyController.showCompanies);
 router.post('/companies', CompanyController.create);
-router.out('/companies/:slug', CompanyController.edit);
+router.put('/companies/:slug', upload.single('image'), CompanyController.edit);
+router.delete('/companies/:slug', CompanyController.delete);
 
 module.exports = router;
